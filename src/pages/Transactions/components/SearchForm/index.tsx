9b99c90ad01d5e1ed02df8c3db617f19dde6bdc4
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-19 22:58:00 -0300

    Capitulo 2 Parte 9

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
     query: z.string()
@@ -15,12 +17,10 @@ export function SearchForm(){
         resolver: zodResolver(searchFormSchema)
     });
 
-    async function handleSearchTransitions(data: SearchFormSchema){
-        await new Promise(resolve => {
-            setTimeout(resolve, 2000);
-        });
-        console.log(data);
+    const {fetchTransactions} = useContext(TransactionsContext);
 
+    async function handleSearchTransitions(data: SearchFormSchema){
+        await fetchTransactions(data.query);
     }
 
     return (
