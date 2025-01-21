9e63417eccd1625112d6706e6b3a4a56a78848c0
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-21 18:46:38 -0300

    Capitulo 3 Parte 2

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 
@@ -34,7 +34,7 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
 
     const [transactions, setTransactions] = useState<Transaction[]>([]);
     
-    async function fetchTransactions(query?: string){
+    const fetchTransactions = useCallback(async (query?: string) => {
         const response = await api.get<Transaction[]>("transactions",{
             params: {
                 _sort: "createdAt",
@@ -43,9 +43,9 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             }
         });
         setTransactions(response.data);
-    }
+    },[]);
 
-    async function createTransaction(data: CreateTransactionData){
+    const  createTransaction = useCallback(async (data: CreateTransactionData) => {
         const {category,description,price,type} = data;
         const response = await api.post("transactions",{
             description,
@@ -55,11 +55,11 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             createdAt: new Date().toISOString()
         });
         setTransactions(prev => [...prev, response.data]);
-    }
+    },[]);
     
     useEffect(() =>{
         fetchTransactions();
-    },[]);
+    },[fetchTransactions]);
 
 
     return (
